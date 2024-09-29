e2454f3bf97e0abcb770bcb37cbbf9c506723dbd
RicardoRpm <Ricardomiguel190@gmail.com>
2024-09-29 15:56:56 +0100

    Update style material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:  [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
